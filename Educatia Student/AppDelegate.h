13bac9a495d2c8249524ef457843278f7ea5a1c0
talaa <tamer.alaaeldine@gmail.com>
2015-08-19 20:23:51 +0400

    Initial Commit

---
+++ Educatia Student/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Educatia Student
+//
+//  Created by Tamer Alaa on 8/19/15.
+//  Copyright (c) 2015 Bluewave Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Educatia Student/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Educatia Student
+//
+//  Created by Tamer Alaa on 8/19/15.
+//  Copyright (c) 2015 Bluewave Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
