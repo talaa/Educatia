2a120f9425778635521acf407be2ad256f13b499
Mena Bebawy <eng.mena.lorans@gmail.com>
2015-09-03 18:05:36 +0300

    GUI Signup view controller

--- Educatia Student/Controller/Sign-Up/SignupViewController.h
+++ Educatia Student/Controller/Sign-Up/SignupViewController.h
@@ -14,7 +14,26 @@
 
 @interface SignupViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
 @property (nonatomic, weak) id<SignupViewControllerDelegate> delegate;
+@property (weak, nonatomic) IBOutlet UITextField *firstNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTexrField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *addressTextField;
 
--(IBAction)dismissViewControllerPress:(id)sender;
+@property (strong, nonatomic) IBOutlet NSString *firstNameString;
+@property (strong, nonatomic) IBOutlet NSString *lastNameString;
+@property (strong, nonatomic) IBOutlet NSString *usernameString;
+@property (strong, nonatomic) IBOutlet NSString *passwordString;
+@property (strong, nonatomic) IBOutlet NSString *confirmPasswordString;
+@property (strong, nonatomic) IBOutlet NSString *phoneString;
+@property (strong, nonatomic) IBOutlet NSString *emailString;
+@property (strong, nonatomic) IBOutlet NSString *addressString;
+
+-(IBAction)submitPressed:(id)sender;
+-(IBAction)dismissViewControllerPressed:(id)sender;
 @end
--- Educatia Student/Controller/Sign-Up/SignupViewController.m
+++ Educatia Student/Controller/Sign-Up/SignupViewController.m
@@ -8,7 +8,7 @@
 
 #import "SignupViewController.h"
 
-@interface SignupViewController ()
+@interface SignupViewController () <UITextFieldDelegate>
 
 @end
 
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self stopActivityIndicator];
+    [self.firstNameTextField addTarget:self action:@selector(checkTextField:) forControlEvents:UIControlEventEditingChanged];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,8 +39,52 @@
     // Pass the selected object to the new view controller.
 }
 */
--(IBAction)dismissViewControllerPress:(id)sender{
+
+- (IBAction)submitPressed:(id)sender {
+    [self startActivityIndicator];
+    
+    if (self.firstNameTextField.text.length >0 && self.lastNameTextField.text.length >0 && self.usernameTextField.text.length >0 && self.passwordTexrField.text.length >0 && self.confirmPasswordTextField.text.length >0 && self.phoneTextField.text.length >0 && self.emailTextField.text.length >0 && self.emailTextField.text.length >0){
+        if ([self.passwordTexrField.text compare:self.confirmPasswordTextField.text] == NSOrderedSame){
+            self.firstNameString    = self.firstNameTextField.text;
+            self.lastNameString     = self.lastNameTextField.text;
+            self.usernameString     = self.usernameTextField.text;
+            self.passwordString     = self.passwordTexrField.text;
+            self.phoneString        = self.phoneTextField.text;
+            self.addressString      = self.addressTextField.text;
+            self.emailString        = self.emailTextField.text;
+        }else{
+            [self stopActivityIndicator];
+            [[[UIAlertView alloc] initWithTitle:@"Education Student" message:@"Kindly entre the same password!" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil] show];
+        }
+    }else{
+        [self stopActivityIndicator];
+        [[[UIAlertView alloc]initWithTitle:@"Education Student" message:@"Kindly make sure type all mandatory data!" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil] show];
+    }
+}
+
+-(IBAction)dismissViewControllerPressed:(id)sender{
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
+#pragma mark - ActivityIndicator Behaviour
+
+- (void)stopActivityIndicator {
+    [self.activityIndicatorView stopAnimating];
+    self.activityIndicatorView.hidden = YES;
+}
+
+- (void)startActivityIndicator {
+    [self.activityIndicatorView startAnimating];
+    self.activityIndicatorView.hidden = NO;
+}
+
+#pragma mark - TextField real time check
+
+-(void)checkTextField:(id)sender {
+    if (self.firstNameTextField.text.length < 4){
+         NSLog(@"Check is Okay");
+    }else {
+        self.firstNameTextField.text = self.firstNameString;
+    }
+}
 @end
