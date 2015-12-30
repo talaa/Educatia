d491b7876b99f7ac4e988aa34e00015c25d0e0dd
Mena Lorans <eng.mena.lorans@gmail.com>
2015-12-30 14:45:46 +0200

    upgrade collection view

---
+++ Educatia Student/Controller/UpgradeApp/UpgradeAppViewController.h
@@ -0,0 +1,16 @@
+//
+//  UpgradeAppViewController.h
+//  Educatia Student
+//
+//  Created by Mena Bebawy on 12/30/15.
+//  Copyright © 2015 Bluewave Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpgradeAppViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *upgradeButton;
+- (IBAction)upgradePressed:(id)sender;
+
+@end
---
+++ Educatia Student/Controller/UpgradeApp/UpgradeAppViewController.m
@@ -0,0 +1,40 @@
+//
+//  UpgradeAppViewController.m
+//  Educatia Student
+//
+//  Created by Mena Bebawy on 12/30/15.
+//  Copyright © 2015 Bluewave Solutions. All rights reserved.
+//
+
+#import "UpgradeAppViewController.h"
+
+@interface UpgradeAppViewController ()
+
+@end
+
+@implementation UpgradeAppViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)upgradePressed:(id)sender{
+    
+}
+@end
